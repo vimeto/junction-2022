e48a5f4e3f25e0c93cb2f7ac9c115fe4e03b3c60
Aapo Leppänen <aapo.leppanen@gmail.com>
2022-11-05 17:04:16 +0200

    impr: fixed feedback

--- components/ui/CheckBox.tsx
+++ components/ui/CheckBox.tsx
@@ -1,14 +1,27 @@
-import { cva, VariantProps } from 'class-variance-authority';
+import { cva, VariantProps } from "class-variance-authority";
+import React from "react";
 
-export interface Props extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {};
+export interface Props
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {}
 
-export function CheckBox({checked, ...props}: Props) {
-  return(
-    <div className="form-control">
-      <label className="label cursor-pointer">
-        <span className="label-text" {...props} /> 
-        <input type="checkbox" checked={checked} className="m-4 checkbox checkbox-md" />
-      </label>
-    </div>
-  )
-}
+export const CheckBox = React.forwardRef<HTMLInputElement, Props>(
+  ({ checked, onChange, ...props }: Props, ref) => {
+    return (
+      <div className="form-control">
+        <label className="label cursor-pointer">
+          <span className="label-text" {...props} />
+          <input
+            type="checkbox"
+            checked={checked}
+            onChange={onChange}
+            className="m-4 checkbox checkbox-md"
+            ref={ref}
+          />
+        </label>
+      </div>
+    );
+  }
+);
